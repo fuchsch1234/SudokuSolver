bb94c0ad7653be4f9b2990ae2bd1d4d267faa45b
cf <abtjh5q@hmamail.com>
2019-01-03 11:56:52 +0100

    Prepare build script for a multi project build

---
+++ sudokusolver/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("application")
+    kotlin("jvm")
+}
+
+application {
+    mainClassName = "${group}.SudokuSolver.SudokuSolver"
+}
+
+dependencies {
+    compile(project(":satsolver-library"))
+    compile(kotlin("stdlib"))
+}
