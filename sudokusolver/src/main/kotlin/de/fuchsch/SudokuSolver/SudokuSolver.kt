89898f43665d33ceadd16ae16eb3e29229a7c9e2
cf <abtjh5q@hmamail.com>
2019-01-03 12:30:39 +0100

    Placeholder app

---
+++ sudokusolver/src/main/kotlin/de/fuchsch/SudokuSolver/SudokuSolver.kt
@@ -0,0 +1,11 @@
+package de.fuchsch.SudokuSolver
+
+class SudokuSolver {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello world")
+        }
+    }
+}
