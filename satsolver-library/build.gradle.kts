8ae417d2df90f90465bc92ceb5bafc14268b07eb
cf <abtjh5q@hmamail.com>
2019-01-14 21:29:19 +0100

    Run and display test results from gradle test

--- satsolver-library/build.gradle.kts
+++ satsolver-library/build.gradle.kts
@@ -7,3 +7,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
 }
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
