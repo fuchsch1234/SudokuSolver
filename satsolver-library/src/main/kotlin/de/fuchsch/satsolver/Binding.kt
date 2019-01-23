6df98379e74fa1a9b0f56e1f42cb8457f2a1d4bf
cf <abtjh5q@hmamail.com>
2019-01-23 07:32:05 +0100

    Document binding class

--- satsolver-library/src/main/kotlin/de/fuchsch/satsolver/Binding.kt
+++ satsolver-library/src/main/kotlin/de/fuchsch/satsolver/Binding.kt
@@ -1,9 +1,20 @@
 package de.fuchsch.satsolver
 
+/**
+ * Models the binding of [Variables][Variable] to boolean values.
+ *
+ * @property boundVariable The mutable binding from Variables to boolean values.
+ */
 data class Binding (
     val boundVariable: MutableMap<Variable, Boolean> = mutableMapOf()
 ) {
 
+    /**
+     * Queries the Binding if a variable is bound by it.
+     *
+     * @param variable The variable to test against this binding.
+     * @return True if the variable is bound by this binding, otherwise false.
+     */
     fun binds(variable: Variable): Boolean = boundVariable.containsKey(variable)
 
 }
