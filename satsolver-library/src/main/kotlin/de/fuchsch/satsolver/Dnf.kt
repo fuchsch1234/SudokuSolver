7d740417666b46fe24f682f66f2fcea0f76b7695
cf <abtjh5q@hmamail.com>
2019-02-16 12:19:13 +0100

    Implement a new class to represent formulas in disjunctive normal form.
    Terms are implemented as separate inner classes, so disjunctive and
    conjunctive terms cannot be mixed.
    
    #4

---
+++ satsolver-library/src/main/kotlin/de/fuchsch/satsolver/Dnf.kt
@@ -0,0 +1,139 @@
+package de.fuchsch.satsolver
+
+/**
+ * Model of a boolean formula in disjunctive normal form, that is a disjunction of disjunction terms.
+ *
+ * @property variables List of variables used in the formulas disjunction terms.
+ * @property terms List of disjunction terms that build the formula.
+ */
+class Dnf (val variables: MutableList<Variable> = mutableListOf()) {
+
+    val terms = mutableListOf<Term>()
+
+    /**
+     * Evaluates this formula against a binding that assigns boolean values to variables.
+     *
+     * The formula is evaluated by evaluating all its terms and combining their [EvaluationResult]s
+     *
+     * @param binding The [Binding] used to assign values to variables.
+     * @return An [EvaluationResult] that represents this formulas evaluation against the binding.
+     */
+    fun evaluate(binding: Binding): EvaluationResult =
+        terms.map { it.evaluate(binding) }.fold(EvaluationResult.FALSE, EvaluationResult::or)
+
+    /**
+     * Adds a term to the formula.
+     *
+     * @param term New term to add.
+     */
+    operator fun plusAssign(term: Term) {
+        terms.add(term)
+    }
+
+    /**
+     * Adds a single variable term.
+     *
+     * @param variable New term contains this one variable.
+     */
+    operator fun plusAssign(variable: Variable) {
+        terms.add(Term() + variable)
+    }
+
+    /**
+     * Creates a new formula identical to the old one plus one new term.
+     *
+     * @param term Additional term for the new formula.
+     * @return A new formula.
+     */
+    operator fun plus(term: Term): Dnf {
+        val newKnf = Dnf()
+        newKnf.terms.addAll(terms)
+        newKnf.terms.add(term)
+        return newKnf
+    }
+
+    /**
+     * Creates a new formula identical to the old one plus one new term with one variable.
+     *
+     * @param term Additional variable for the new formula.
+     * @return A new formula.
+     */
+    operator fun plus(variable: Variable): Dnf {
+        val newKnf = Dnf()
+        newKnf.terms.addAll(terms)
+        newKnf += variable
+        return newKnf
+    }
+
+    /**
+     * Model for a single disjunction term used to build [Dnf] formulas.
+     *
+     * A term evaluates to [EvaluationResult.FALSE] under a [Binding], iff all positive variables in the term
+     * evaluate to false and all negated variables evaluate to true.
+     * Iff at least one positive variable evaluates to true or at least one negated variable evaluates
+     * to false, the term evaluates [EvaluationResult.TRUE].
+     * Iff the term is neither true nor false it is [EvaluationResult.UNDEFINED]
+     *
+     * @property positiveVariables List of non negated variables in the term.
+     * @property negativeVariables List of negated variables in the term.
+     */
+    data class Term (
+        val positiveVariables: MutableList<Variable> = mutableListOf(),
+        val negativeVariables: MutableList<Variable> = mutableListOf()
+    ) {
+
+        /**
+         * Evaluates this term against a [Binding] to a boolean value or [EvaluationResult.UNDEFINED].
+         *
+         * @param binding The binding that assigns boolean values to the variables in this term.
+         * @return The result of evaluating this term against the binding.
+         */
+        fun evaluate(binding: Binding): EvaluationResult =
+            positiveVariables.map { binding.evaluate(it) }.fold(EvaluationResult.TRUE, EvaluationResult::and).and(
+                negativeVariables.map { binding.evaluate(it) }.fold(EvaluationResult.TRUE) { acc, v -> acc.and(!v) }
+            )
+
+        /**
+         * Creates a new term with an additional non negated variable.
+         *
+         * @param v The new variable that is present in the new term.
+         * @return A new term that contains all of this variables and the new one.
+         */
+        operator fun plus(v: Variable): Term {
+            val newTerm = this.copy()
+            newTerm.positiveVariables.add(v)
+            return newTerm
+        }
+
+        /**
+         * Creates a new term with an additional negated variable.
+         *
+         * @param v The new variable that is present in the new term.
+         * @return A new term that contains all of this variables and the new one.
+         */
+        operator fun minus(v: Variable): Term {
+            val newTerm = this.copy()
+            newTerm.negativeVariables.add(v)
+            return newTerm
+        }
+
+        /**
+         * Adds a new non negated variable to this term.
+         *
+         * @param v The new variable.
+         */
+        operator fun plusAssign(v: Variable) {
+            positiveVariables.add(v)
+        }
+
+        /**
+         * Adds a new negated variable to this term.
+         *
+         * @param v The new variable
+         */
+        operator fun minusAssign(v: Variable) {
+            negativeVariables.add(v)
+        }
+    }
+
+}
