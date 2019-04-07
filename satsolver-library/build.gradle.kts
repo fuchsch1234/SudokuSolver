17b8cd12d2fb26103780a54cd050b087940a62e5
cf <abtjh5q@hmamail.com>
2019-04-07 15:23:15 +0200

    Add arrow library dependency to satsolver library

--- satsolver-library/build.gradle.kts
+++ satsolver-library/build.gradle.kts
@@ -2,12 +2,22 @@ plugins {
     kotlin("jvm")
     kotlin("kapt")
     id("org.jetbrains.dokka")
+    idea
 }
 
 dependencies {
+    val arrow_version = "0.9.0"
+
     compile(kotlin("stdlib"))
+
     compile("org.slf4j:slf4j-api:1.7.25")
     compile("org.slf4j:slf4j-log4j12:1.7.26")
+
+    compile("io.arrow-kt:arrow-core-data:$arrow_version")
+    compile("io.arrow-kt:arrow-optics:$arrow_version")
+    kapt("io.arrow-kt:arrow-optics:$arrow_version")
+    kapt("io.arrow-kt:arrow-meta:$arrow_version")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.1.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.1.0")
 }
@@ -16,5 +26,26 @@ tasks.withType<Test> {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
+    }
+}
+
+idea {
+    module {
+        sourceDirs.addAll(files(
+                "build/generated/source/kapt/main",
+                "build/generated/source/kapt/debug",
+                "build/generated/source/kapt/release",
+                "build/generated/source/kaptKotlin/main",
+                "build/generated/source/kaptKotlin/debug",
+                "build/generated/source/kaptKotlin/release",
+                "build/tmp/kapt/main/kotlinGenerated"))
+        generatedSourceDirs.addAll(files(
+                "build/generated/source/kapt/main",
+                "build/generated/source/kapt/debug",
+                "build/generated/source/kapt/release",
+                "build/generated/source/kaptKotlin/main",
+                "build/generated/source/kaptKotlin/debug",
+                "build/generated/source/kaptKotlin/release",
+                "build/tmp/kapt/main/kotlinGenerated"))
     }
 }
